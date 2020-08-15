f52b6383140745ad5defc3eaa6836b0d12fc9c86
andpberr <andrew.berry91@gmail.com>
2020-08-15 18:47:47 -0500

    initial commit

---
+++ game.js
@@ -0,0 +1,19 @@
+import foo from '/.helpers';
+
+function draw() {
+    let ctx = getCanvasContext();
+    fillBackground(ctx);
+    console.log(foo);
+}
+
+function getCanvasContext() {
+    let canv = document.getElementById("game");
+    console.log(canv);
+    return canv.getContext("2d");
+    
+}
+
+function fillBackground(ctx) {
+    ctx.fillStyle = "snow";
+    ctx.fillRect(0,0,640,480);
+}
---
+++ helpers.js
@@ -0,0 +1,2 @@
+const foo = 'hello';
+export default foo;
